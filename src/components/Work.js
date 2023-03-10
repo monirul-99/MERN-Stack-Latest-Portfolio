f1bbe5cf551d4988d86817fc92bd308a0ae7ac04
monaulic <sohelmuhammad164@gmail.com>
2023-03-10 23:15:21 +0600

    work projects sample add

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,97 @@
 import React from "react";
 
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
+//images
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
+
 const Work = () => {
   return (
-    <div id="work" className="section">
-      Work
-    </div>
+    <section id="work" className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+            {/* text area */}
+            <div>
+              <h2 className="h2 leading-tight text-accent">
+                My latest <br />
+                Work
+              </h2>
+              <p className="max-w-sm mb-16">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
+                aperiam neque. Animi unde dolorem quos facere enim at, saepe
+                corporis.
+              </p>
+              <button className="btn btn-sm capitalize">
+                View all projects
+              </button>
+            </div>
+            {/* images */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img1}
+                alt=""
+              />
+              {/* pre title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">Front-End Developer</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Projects Title</span>
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 flex flex-col gap-y-10">
+            {/* images2 */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img1}
+                alt=""
+              />
+              {/* pre title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">Front-End Developer</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Projects Title</span>
+              </div>
+            </div>
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img1}
+                alt=""
+              />
+              {/* pre title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">Front-End Developer</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Projects Title</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
