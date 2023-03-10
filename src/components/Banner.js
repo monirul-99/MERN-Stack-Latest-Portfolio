1694ba8dacdbd969a34358a1c6b39392d7109f39
monaulic <sohelmuhammad164@gmail.com>
2023-03-10 19:56:10 +0600

    almost complete cover area

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import React from "react";
 
+//images import here
+import Image from "../assets/avatar.svg";
+//Icons Import here
+import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
+//type image animation
+import { TypeAnimation } from "react-type-animation";
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className="section" id="home">
+      <div className="container mx-auto">
+        <div>
+          {/* text area*/}
+          <div>
+            <h1>
+              Monirul <span>Islam</span>
+            </h1>
+            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+              <span className="mr-4">I'm a</span>
+              <TypeAnimation
+                sequence={[
+                  "MERN Stack Developer",
+                  2000,
+                  "React JS Developer",
+                  2000,
+                  "Full Stack Developer",
+                  2000,
+                ]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </div>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Blanditiis corrupti perferendis at non quas sed libero debitis,
+              reprehenderit quibusdam.
+            </p>
+            <div>
+              <button>Conatct Me</button>
+              <a href="#">My Portfolio</a>
+            </div>
+          </div>
+          {/* images area */}
+          <div>
+            <img src={Image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,12 @@ import React from "react";
 //import icon
 
 import { BiHomeAlt, BiUser } from "react-icons/bi";
-import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+import {
+  BsClipboardData,
+  BsBriefcase,
+  BsChatSquare,
+  BsChatSquareText,
+} from "react-icons/bs";
 import { Link } from "react-scroll";
 
 const Nav = () => {
@@ -12,17 +17,51 @@ const Nav = () => {
       <div className="container mx-auto">
         {/* Navbar part */}
         <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="home"
+            activeClass="active"
+            offset={-200}
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BiHomeAlt />
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="about"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BiUser />
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="services"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BsClipboardData />
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="work"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BsBriefcase />
+          </Link>
+          <Link
+            to="contact"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BsChatSquareText />
           </Link>
         </div>
       </div>
