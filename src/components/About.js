0f816b8b0acd21661508ca527b5ea8b360766307
monaulic <sohelmuhammad164@gmail.com>
2023-03-10 22:16:02 +0600

    service section working

--- src/components/About.js
+++ src/components/About.js
@@ -16,17 +16,29 @@ const About = () => {
   return (
     <section id="about" className="section" ref={ref}>
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           {/* img area */}
-          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+          ></motion.div>
           {/* text area */}
-          <div>
-            <h2>About me.</h2>
-            <h3>
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent">About me.</h2>
+            <h3 className="h3 mb-4">
               I am a quick learner and am always eager to expand my skill set
               and stay up-to-date with the latest technologies.{" "}
             </h3>
-            <p>
+            <p className="mb-6">
               I have experience building web applications using the MERN stack,
               including knowledge of MongoDB, Express.js, React.js Redux JS, and
               Node.js. I also have a strong understanding of front-end
@@ -64,7 +76,15 @@ const About = () => {
                 </div>
               </div>
             </div>
-          </div>
+
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient text-link">
+                {" "}
+                My Portfolio
+              </a>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,72 @@
 import React from "react";
+//icons
+import { BsArrowUpRight } from "react-icons/bs";
+//motion
+import {} from "framer-motion";
+//variant
+import { fadeIn } from "../variants";
 
+// service Data
+const serviceData = [
+  {
+    name: "Shop Ex Website",
+    description:
+      "I have experience building web applications using the MERN stack, including knowledge of MongoDB, Express.js, React.js Redux JS, and Node.js.",
+    link: "learn more",
+  },
+  {
+    name: "Brands Plants Website",
+    description:
+      "I have experience building web applications using the MERN stack, including knowledge of MongoDB, Express.js, React.js Redux JS, and Node.js.",
+    link: "learn more",
+  },
+  {
+    name: "Communication Website",
+    description:
+      "I have experience building web applications using the MERN stack, including knowledge of MongoDB, Express.js, React.js Redux JS, and Node.js.",
+    link: "learn more",
+  },
+];
 const Services = () => {
   return (
-    <div id="services" className="section">
-      Services
-    </div>
+    <section id="services" className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* text & images */}
+          <div className="flex-1 lg:bg-services lg:bg-no-repeat mix-blend-lighten mb-12 lg:mb-0">
+            <h2 className="h2 text-accent mb-6">What I Do</h2>
+            <h3 className="h3 max-w-[455px] mb-16">
+              I have experience building web applications using the MERN
+              stack,including knowledge of MongoDB, Express.js, React.js Redux
+              JS, and Node.js.
+            </h3>
+            <button className="btn btn-sm">See My Works</button>
+          </div>
+
+          <div>
+            {serviceData.map((service, index) => {
+              const { name, description } = service;
+              return (
+                <div
+                  className="border-b border-white/20 h-[146px] mb-[38px] flex"
+                  key={index}
+                >
+                  <div className="max-w-[476px]">
+                    <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                      {name}
+                    </h4>
+                    <p className="font-secondary leading-tight">
+                      {description}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+            service
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
